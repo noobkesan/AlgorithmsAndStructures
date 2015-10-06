be8c2b08da39bfafc2fa974497f5b530a0b17b83
avnik <avnik@efo.ru>
2015-10-06 16:37:33 +0300

    shell sort

---
+++ src/main/java/ru/algorithms/sort/shell/ShellSort.java
@@ -0,0 +1,40 @@
+package ru.algorithms.sort.shell;
+
+import ru.algorithms.Util;
+
+import java.util.Arrays;
+
+/**
+ * O(N ^ (3/2))
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] array = Util.defaultArray();
+        System.out.println("Before sort: " + Arrays.toString(array));
+        sort(array);
+        System.out.println("After sort: " + Arrays.toString(array));
+    }
+
+    private static void sort(int[] array) {
+        int h = 1;
+        while (h <= array.length / 3) {
+            h = 3 * h + 1;
+        }
+
+        while (h > 0) {
+
+            for (int i = h; i < array.length; i++) {
+                int temp = array[i];
+                int j = i;
+                while (j > h - 1 && array[j - h] >= temp) {
+                    array[j] = array[j - h];
+                    j -= h;
+                }
+
+                array[j] = temp;
+            }
+
+            h = (h - 1) / 3;
+        }
+    }
+}
