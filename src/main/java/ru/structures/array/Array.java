59bb0fc8c68a37898576289836213a66b7296315
avnik <avnik@avnik.com>
2016-04-11 21:50:55 +0300

    remove structures

--- src/main/java/ru/structures/array/Array.java
+++
@@ -1,7 +0,0 @@
-package ru.structures.array;
-
-public interface Array<T> {
-    T find(T element);
-    boolean insert(T element);
-    boolean delete(T element);
-}
--- src/main/java/ru/structures/tree/Tree.java
+++
@@ -1,8 +0,0 @@
-package ru.structures.tree;
-
-public interface Tree {
-    public void find(int key);
-    public void insert(int key);
-    public void delete(int key);
-    public void print();
-}
